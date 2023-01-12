9fb323c6f9bd026b09aa5c5f167265ca4d180903
kevinflynn <flynny85@gmail.com>
2023-01-12 20:00:04 +0000

    lint fixes

--- migrations/20230105141649_add-name.js
+++ migrations/20230105141649_add-name.js
@@ -3,17 +3,17 @@
  * @returns { Promise<void> }
  */
 export async function up(knex) {
-    await knex.schema.alterTable('users', function (table) {
+  await knex.schema.alterTable('users', function (table) {
     table.string('name')
   })
-};
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 export async function down(knex) {
-    await knex.schema.alterTable('users', function (table) {
+  await knex.schema.alterTable('users', function (table) {
     table.dropColumn('name')
   })
-};
+}
