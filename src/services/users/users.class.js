a6a569b9cd63cf5764d4913128a3a66cc23341ac
kevinflynn <flynny85@gmail.com>
2023-01-26 16:30:04 +0000

    block deleted user from logging in

--- src/services/users/users.class.js
+++ src/services/users/users.class.js
@@ -1,7 +1,18 @@
 import { KnexService } from '@feathersjs/knex'
+import { NotFound } from '@feathersjs/errors'
 
 // By default calls the standard Knex adapter service methods but can be customized with your own functionality.
 export class UserService extends KnexService {
+  async get(id, params) {
+    const result = await super.get(id, params)
+
+    if (result.deleted_at) {
+      throw new NotFound('User removed')
+    }
+
+    return result
+  }
+
   async remove(id, params) {
     return super.patch(id, { deleted_at: new Date() }, params)
   }
